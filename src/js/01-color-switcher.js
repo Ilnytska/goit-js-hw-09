58141fec1d3941abe9063af286c27d8521e73604
Ilnytska <olly.happyfly@gmail.com>
2021-12-22 23:15:30 +0200

    Update 01-color-switcher.js
    add solution of task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
-console.log('4')
+refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+let timerId = null;
+
+function onStartClick() {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor()
+    document.body.style.background = color;
+
+  }, 1000);
+    refs.stopBtn.removeAttribute('disabled');
+    refs.startBtn.setAttribute('disabled', true); 
+}
+
+function onStopClick() {
+    clearInterval(timerId);
+       refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
